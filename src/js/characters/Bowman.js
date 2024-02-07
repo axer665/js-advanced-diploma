50fdd05768e33c1156217ec221f6b58c135d50a4
Misha <vellsevul@mail.ru>
2024-02-07 11:00:31 +0300

    Publish

--- src/js/characters/Bowman.js
+++
@@ -1,9 +0,0 @@
-import Character from '../Character';
-
-export default class Bowman extends Character {
-    constructor(level) {
-        super(level, 25, 25, 'Bowman');
-        this.moveDistance = 2;
-        this.attackDistance = 2;
-    }
-}
--- src/js/characters/Daemon.js
+++
@@ -1,9 +0,0 @@
-import Character from '../Character';
-
-export default class Daemon extends Character {
-    constructor(level) {
-        super(level, 10, 10, 'Daemon');
-        this.moveDistance = 1;
-        this.attackDistance = 4;
-    }
-}
